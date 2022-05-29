28cf74bebb39517c27313c85136b2d8a2fe20e15
lai <bycheng.lai@gmail.com>
2022-05-30 01:51:23 +0800

    * 更新 CategoryController * 更新表單功能

--- app/Controllers/CategoryController.php
+++ app/Controllers/CategoryController.php
@@ -8,8 +8,45 @@ class CategoryController extends BaseController {
 
     public function deleteCategory($request, $response, $args)
     {
+        $category_id = $args['id'];
+        // TO DO: delete category by id from the database
     
         $response = $this->container->view->render($response, 'view.phtml',['category_deleted'=>true]);
+
+        return $response;
+    }
+
+    public function showCategory($request, $response, $args)
+    {
+        $category_id = $args['id'];
+        // TO DO: get category by id from the database
+        $category = 'Electronics';
+    
+        $response = $this->container->view->render($response, 'view.phtml',['category'=>$category]);
+
+        return $response;
+    }
+
+    public function editCategory($request, $response, $args)
+    {
+        $category_id = $args['id'];
+        // TO DO: get category by id from the database
+        $category = ['name'=>'Electronics','parent'=>null];
+    
+        $response = $this->container->view->render($response, 'view.phtml',['editedCategory'=>$category]);
+
+        return $response;
+    }
+
+    public function saveCategory($request, $response, $args)
+    {
+        $data = $request->getParsedBody();
+        if(empty($data['category_name']) || empty($data['category_description']))
+        $categorySaved = false;
+        else
+        $categorySaved = true;
+
+        $response = $this->container->view->render($response, 'view.phtml',['categorySaved'=>$categorySaved]);
 
         return $response;
     }
